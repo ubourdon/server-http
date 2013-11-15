5ac3c7a1bcfc1e14b53de978d46d083553c77842
ubourdon <bourdon.ugo@gmail.com>
2013-11-15 18:09:27 +0100

    initial commit

---
+++ project/SimplyScalaHttpBuild.scala
@@ -0,0 +1,22 @@
+import sbt._
+import sbt.Keys._
+
+object ServerHttpBuild extends Build {
+    // make library => 'sbt + package' & 'sbt + make-pom'
+
+    lazy val root = Project(id = "server-http", base = file("."),
+        settings = Project.defaultSettings ++ Seq(
+            name := "server-http",
+
+            description := "server http nio & akka based",
+
+            version := "0.1-SNAPSHOT",
+
+            scalaVersion := "2.10.2",
+
+            libraryDependencies ++= Seq(
+                "org.scalatest"            %% "scalatest"          % "2.0"       % "test"
+            )
+        )
+    )
+}
